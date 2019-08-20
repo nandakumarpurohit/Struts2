75862d2737b473c20ff9549856f39849f8ea957c
nandakumarpurohit <nandakumarpurohit@gmail.com>
2019-08-20 07:34:41 -0400

    init files

---
+++ hello-struts/src/com/demo/action/HelloAction.java
@@ -0,0 +1,12 @@
+package com.demo.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloAction extends ActionSupport {
+	
+	public String execute() {
+		
+		return SUCCESS;
+	}
+
+}
